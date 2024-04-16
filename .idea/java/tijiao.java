f3d9788bf64a01cf0d651e013c631a7e27fa7c32
zgl <2112549322@qq.com>
2024-04-16 15:36:08 +0800

    zuoye

---
+++ .idea/java/tijiao.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("姓名:张高亮");
+        System.out.println("班级:22物联网04");
+        System.out.println("学号:66666");
+
+    }
+}
