df9ae3f380ed5559ad6fee1805375146217820c0
zgl <2112549322@qq.com>
2024-04-16 15:36:08 +0800

    zuoye

---
+++ .idea/java/tijiao.java
@@ -0,0 +1,20 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("姓名:");
+        System.out.println("班级:");
+        System.out.println("学号:");
+        System.out.println("日期:");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+                
